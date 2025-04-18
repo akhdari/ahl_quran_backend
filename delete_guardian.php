f037575220351a3417ba9d109fc5b1c7b1eff294
akhdari <120687076+akhdari@users.noreply.github.com>
2025-04-18 18:08:34 +0200

    add delete guardian

---
+++ delete_guardian.php
@@ -0,0 +1,49 @@
+<?php
+include './connect.php';
+include_once './cors.php';
+include_once './add_guardian.php';
+
+function get_guardian()
+{
+    $jsonData = file_get_contents("php://input");
+    $data = json_decode($jsonData, true);
+
+    if (!$data) {
+        http_response_code(400);
+        echo json_encode(['success' => false, 'message' => 'No JSON data received or format is invalid']);
+        return null;
+    }
+    return $data;
+}
+
+function delete_guardian($guardian_id, $conn )
+{
+    $stmt = $conn->prepare("DELETE FROM guardian WHERE guardian_id = ?");
+    $stmt->bind_param("s", $guardian_id);
+    $stmt->execute();
+    $stmt->close();
+}
+
+$data = get_guardian();
+if ($data) {
+    try {
+        $guardian_id = $data['id'] ?? null;
+        if (empty($guardian_id)) {
+            throw new Exception("Guardian ID is required.");
+        }
+        delete_guardian($guardian_id, $conn);
+        echo json_encode(['success' => true, 'message' => 'Guardian deleted successfully']);
+
+
+    } catch (Exception $e) {
+        http_response_code(400);
+        echo json_encode([
+            'success' => false,
+            'message' => $e->getMessage()
+        ]);
+    }
+} else {
+    http_response_code(400);
+    echo json_encode(['success' => false, 'message' => 'Invalid or missing data format']);
+}
+?>
--- query.php
+++ query.php
@@ -9,8 +9,8 @@ $query1 =
        lecture.lecture_name_ar, 
        account_info.username
 FROM student 
-INNER JOIN personal_info ON student.student_id = personal_info.student_id
-INNER JOIN account_info ON student.student_id = account_info.account_id
+LEFT JOIN personal_info ON student.student_id = personal_info.student_id
+LEFT JOIN account_info ON student.student_id = account_info.account_id
 LEFT JOIN lecture_student ON student.student_id = lecture_student.student_id  
 LEFT JOIN lecture ON lecture_student.lecture_id = lecture.lecture_id;";
 $query2 = 
@@ -27,6 +27,7 @@ GROUP BY lecture.lecture_name_ar, lecture.circle_type;
 
 $query3=
 "SELECT 
+    guardian.guardian_id,
     guardian.last_name, 
     guardian.first_name,  
     guardian.date_of_birth, 
@@ -34,7 +35,6 @@ $query3=
     contact_info.phone_number, 
     contact_info.email, 
     guardian_account.username AS guardian_account, 
-    student_account.username AS student_account,
     GROUP_CONCAT(student_account.username SEPARATOR ', ') AS children
 FROM guardian
 LEFT JOIN contact_info 
@@ -46,6 +46,7 @@ LEFT JOIN student
 LEFT JOIN account_info AS student_account 
     ON student.student_account_id = student_account.account_id
 GROUP BY guardian.guardian_id;";
+
 $query4="SELECT lecture_name_ar FROM lecture;";
 $query5 = "SELECT username FROM account_info INNER JOIN guardian WHERE guardian_account_id = account_id;";
 $query6 = "SELECT teacher_id, CONCAT(first_name, ' ', last_name) AS full_name FROM teacher;";
